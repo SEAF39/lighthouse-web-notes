41b564f467df52893f1e81af2ab6fdee855c7866
SEAF39 <seaf.roshi.37@gmail.com>
2023-01-31 23:18:26 +0000

    corrected function

--- week_1/day_1/d1-focal/Debugging/reverse.js
+++ week_1/day_1/d1-focal/Debugging/reverse.js
@@ -1,3 +1,7 @@
+/*
+
+The reverse function has a couple of errors. The variable orignal is mispelled and should be original. The method reverseList is also not a valid JavaScript method, and it should be reverse.
+
 
 var input = process.argv[2];
 
@@ -6,4 +10,23 @@ if (input)
 
 function reverse(original) {
   return orignal.split('').reverseList().join('');
+}
+
+*/
+
+
+// The corrected code here :-
+// retrieve the third argument from the command line
+var input = process.argv[2];
+
+// check if an input exists
+if (input) {
+  // if input exists, log the result of reversing the input
+  console.log(reverse(input));
+}
+
+// define the reverse function
+function reverse(original) {
+  // split the original string into an array of characters, reverse the order of the array, and then join it back into a string
+  return original.split('').reverse().join('');
 }
