8a90de4568e1cb45ab78627a11620a5481dea4a6
SEAF39 <seaf.roshi.37@gmail.com>
2023-01-31 23:08:08 +0000

    This code has error

--- week_1/day_1/d1-focal/Debugging/average.js
+++ week_1/day_1/d1-focal/Debugging/average.js
@@ -1,3 +1,8 @@
+
+/*
+
+// The given code contains a syntax error in the for-of loop, where ++ has been mistakenly added before num. This will cause the loop to increment the value of num before it's even used in the calculation, leading to incorrect results. To fix this, the line sum ++ num; should be changed to sum += num;.
+
 function average(list) {
   var sum = 0;
 
@@ -7,4 +12,22 @@ function average(list) {
 
   return sum / list.length;
 
+console.log(average([3, 5, 7]));
+*/
+
+function average(list) {
+  // declare a variable to store the sum of the numbers
+  var sum = 0;
+
+  // loop through each number in the list
+  for (var num of list) {
+    // add the current number to the sum
+    sum += num;
+  }
+
+  // return the average by dividing the sum by the length of the list
+  return sum / list.length;
+}
+
+// log the result of the average function with an example array of numbers
 console.log(average([3, 5, 7]));
