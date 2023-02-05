a1462f3af16dee59ad70e5740bcf62def7b30677
SEAF39 <seaf.roshi.37@gmail.com>
2023-02-05 20:57:47 +0000

    Exercise 1 - Callback Arguments -Module-1-Week2

---
+++ week_2/focal/functions/callbacks-intro/findWaldo.js
@@ -0,0 +1,27 @@
+
+// Exercise 1 - Callback Arguments
+
+
+// define the findWaldo function that takes in an array and a callback function as arguments
+function findWaldo(arr, callback) {
+  // loop through the elements of the array
+  for (var i = 0; i < arr.length; i++) {
+    // if the current element is "Waldo", call the callback function with the index passed as an argument
+    if (arr[i] === "Waldo") {
+      callback(i);
+      // break the loop once the callback function has been called
+      break;
+    }
+  }
+}
+
+// define the actionWhenFound function that takes in an index as an argument
+function actionWhenFound(index) {
+  // log a message with the index passed to the function
+  console.log("Found Waldo at index " + index + "!");
+}
+
+// call the findWaldo function with an array and the actionWhenFound function as arguments
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
