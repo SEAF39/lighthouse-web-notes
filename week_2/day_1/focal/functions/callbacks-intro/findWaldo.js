2b94edce553070412b613ebb127a5d6f3bc80814
SEAF39 <seaf.roshi.37@gmail.com>
2023-02-05 22:09:27 +0000

    Exercise 1&2 - Callback Arguments-ArrayFor each -Module-1-Week2

--- week_2/day_1/focal/functions/callbacks-intro/findWaldo.js
+++ week_2/day_1/focal/functions/callbacks-intro/findWaldo.js
@@ -3,6 +3,7 @@
 // Module_1 - Week2
 
 // define the findWaldo function that takes in an array and a callback function as arguments
+/*
 function findWaldo(arr, callback) {
   // loop through the elements of the array
   for (let i = 0; i < arr.length; i++) {
@@ -24,3 +25,31 @@ function actionWhenFound(index) {
 // call the findWaldo function with an array and the actionWhenFound function as arguments
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
+*/
+
+
+// Exercise 2 - Array forEach
+// define the actionWhenFound function that takes in an index as an argument
+
+function actionWhenFound(index) {
+  // log a message with the index passed to the function
+  console.log("Found Waldo at index " + index + "!");
+}
+
+// call the findWaldo function with an array and the actionWhenFound function as arguments
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+function findWaldo(arr, callback) {
+  arr.forEach(function(element, index) {
+    if (element === "Waldo") {
+      callback(index);
+    }
+  });
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
