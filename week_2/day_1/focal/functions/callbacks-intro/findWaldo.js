a0f0b6830ac851c734fce6f77cb516ef330363a7
SEAF39 <seaf.roshi.37@gmail.com>
2023-02-05 22:30:21 +0000

    Exercise 1&2 - Fix error function style - module1-day1

--- week_2/day_1/focal/functions/callbacks-intro/findWaldo.js
+++ week_2/day_1/focal/functions/callbacks-intro/findWaldo.js
@@ -3,8 +3,8 @@
 // Module_1 - Week2
 
 // define the findWaldo function that takes in an array and a callback function as arguments
-/*
-function findWaldo(arr, callback) {
+
+const findWaldo = function(arr, callback) {
   // loop through the elements of the array
   for (let i = 0; i < arr.length; i++) {
     // if the current element is "Waldo", call the callback function with the index passed as an argument
@@ -14,42 +14,37 @@ function findWaldo(arr, callback) {
       break;
     }
   }
-}
+};
 
 // define the actionWhenFound function that takes in an index as an argument
-function actionWhenFound(index) {
+const actionWhenFound = function(index) {
   // log a message with the index passed to the function
   console.log("Found Waldo at index " + index + "!");
-}
+};
 
 // call the findWaldo function with an array and the actionWhenFound function as arguments
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-*/
 
 
 // Exercise 2 - Array forEach
 // define the actionWhenFound function that takes in an index as an argument
 
-function actionWhenFound(index) {
-  // log a message with the index passed to the function
-  console.log("Found Waldo at index " + index + "!");
-}
-
 // call the findWaldo function with an array and the actionWhenFound function as arguments
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-
-function findWaldo(arr, callback) {
+/*
+const findWaldo = (arr, callback) => {
   arr.forEach(function(element, index) {
     if (element === "Waldo") {
       callback(index);
     }
   });
-}
+};
 
-function actionWhenFound(index) {
+const actionWhenFound = index => {
   console.log("Found Waldo at index " + index + "!");
-}
+};
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+*/
+
+
