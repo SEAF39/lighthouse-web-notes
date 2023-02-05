0b2dc81b2cec5f9ff9e71c6f524fa7b615545bb0
SEAF39 <seaf.roshi.37@gmail.com>
2023-02-05 22:33:27 +0000

    Exercise 2 - Fix error function style - module1-day1

--- week_2/day_1/focal/functions/callbacks-intro/findWaldo.js
+++ week_2/day_1/focal/functions/callbacks-intro/findWaldo.js
@@ -3,7 +3,7 @@
 // Module_1 - Week2
 
 // define the findWaldo function that takes in an array and a callback function as arguments
-
+/*
 const findWaldo = function(arr, callback) {
   // loop through the elements of the array
   for (let i = 0; i < arr.length; i++) {
@@ -22,15 +22,16 @@ const actionWhenFound = function(index) {
   console.log("Found Waldo at index " + index + "!");
 };
 
+
 // call the findWaldo function with an array and the actionWhenFound function as arguments
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
+*/
 
 // Exercise 2 - Array forEach
 // define the actionWhenFound function that takes in an index as an argument
 
 // call the findWaldo function with an array and the actionWhenFound function as arguments
-/*
+
 const findWaldo = (arr, callback) => {
   arr.forEach(function(element, index) {
     if (element === "Waldo") {
@@ -45,6 +46,5 @@ const actionWhenFound = index => {
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
-*/
 
 
