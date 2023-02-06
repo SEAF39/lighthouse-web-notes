048eadd4525d4aeabb21f32b99450eb8fe0ea61f
SEAF39 <seaf.roshi.37@gmail.com>
2023-02-06 05:37:53 +0000

    Exercise - Refactor findWaldo - module1-day1

--- week_2/day_1/focal/functions/callbacks-intro/findWaldoAnonymously.js
+++ week_2/day_1/focal/functions/callbacks-intro/findWaldoAnonymously.js
@@ -1,5 +1,6 @@
 // Anonymous Functions Excercise //
 
+/*
 // define the findWaldo function that takes in an array and a callback function as arguments
 const findWaldo = (arr, callback) => {
   // loop through the elements of the array
@@ -17,4 +18,25 @@ const findWaldo = (arr, callback) => {
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {
   // log a message with the index passed to the function
   console.log("Found Waldo at index " + index + "!");
+});
+*/
+
+// Exercise - Refactor findWaldo
+
+// Define the findWaldo function that takes an array and a callback function as arguments
+const findWaldo = (arr, callback) => {
+  // Use forEach method to iterate through the elements of the array
+  arr.forEach(function(element, index) {
+    // If the element is "Waldo"
+    if (element === "Waldo") {
+      // Invoke the callback function and pass in the index as an argument
+      callback(index);
+    }
+  });
+};
+
+// Call the findWaldo function and pass in an anonymous callback function as an argument
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {
+  // Log the index to the console
+  console.log("Waldo is located at index:", index);
 });
