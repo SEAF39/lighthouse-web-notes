aee77c616d3a99ff127e7f7a4baff6987bc01d28
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-15 19:27:08 -0500

    GettersAndSetters - Refactoring the code - Exercise - MOUDEL 1- WEEK- 4

--- week_4/day_1/oop/GettersAndSetters.js
+++ week_4/day_1/oop/GettersAndSetters.js
@@ -1,3 +1,5 @@
+
+/*
 class Pizza {
   constructor() {
     this.toppings = [];
@@ -51,3 +53,64 @@ pizza.size = 'm';
 console.log(pizza.getSize()); // m
 
 console.log(pizza.price); // 14
+
+
+*/
+
+// Refactoring the code based on the exercise  inputs 
+class Pizza {
+  constructor() {
+    this.toppings = [];
+    this._size = 'm'; // we use _size to avoid conflicts with the size setter method
+  }
+
+  // adds a topping to the toppings array
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+
+  // removes a topping from the toppings array
+  removeTopping(topping) {
+    const index = this.toppings.indexOf(topping);
+    if (index !== -1) {
+      this.toppings.splice(index, 1);
+    }
+  }
+
+  // returns the size of the pizza
+  getSize() {
+    return this._size;
+  }
+
+  // sets the size of the pizza, with data validation
+  setSize(size) {
+    if (size === 's' || size === 'm' || size === 'l') {
+      this._size = size;
+    }
+  }
+
+  // returns the price of the pizza, computed on the fly based on toppings and size
+  get price() {
+    const basePrice = 10;
+    const toppingPrice = 2;
+    return basePrice + (this.toppings.length * toppingPrice);
+  }
+}
+
+// DRIVER CODE
+let pizza = new Pizza();
+console.log(pizza.getSize()); // 'm'
+
+pizza.setSize('s');
+console.log(pizza.getSize()); // 's'
+
+console.log(pizza.price); // 10, base price for a small pizza with no toppings
+
+pizza.addTopping('mushrooms');
+console.log(pizza.price); // 12, base price plus 2 dollars for one topping
+
+pizza.addTopping('peppers');
+console.log(pizza.price); // 14, base price plus 4 dollars for two toppings
+
+pizza.removeTopping('mushrooms');
+console.log(pizza.price); // 12, base price plus 2 dollars for one topping
