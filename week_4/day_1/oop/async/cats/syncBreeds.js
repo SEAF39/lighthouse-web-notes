b1b8369343ad11b2b35c1aeab932e4d8898be14f
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-21 01:21:12 -0500

    - reafactoring syncBreeds with comments - MOUDEL 1- WEEK- 4

--- week_4/day_1/oop/async/cats/syncBreeds.js
+++ week_4/day_1/oop/async/cats/syncBreeds.js
@@ -1,17 +1,25 @@
 // define an object containing cat breeds and their descriptions
 const catBreeds = {
-  'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
-  'Bombay': "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
+  Balinese:
+  
+  "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
+  
+  Bombay: 
+  
+  "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
 };
 
 // define a function to get the description of a cat breed
+
 const breedDetails = function(breed) {
-  // return the description of the specified breed
+
+  // return the description of the specified breed from the catBreeds object
+
   return catBreeds[breed];
+
 };
 
-// get the description for the 'Bombay' breed by calling the breedDetails function
-const bombay = breedDetails('Bombay');
 
-// log the description of the 'Bombay' breed to the console
-console.log(bombay);
+// export the breedDetails function so that it can be used in other modules
+
+module.exports = breedDetails;
