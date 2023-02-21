7c8099093e65ffd5d8f794ec1adcfeeec9ab5017
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-20 21:25:25 -0500

    - setTimeout Practice - Part || - MOUDEL 1- WEEK- 4

--- week_4/day_1/oop/async/hello_timeout.js
+++ week_4/day_1/oop/async/hello_timeout.js
@@ -1,3 +1,8 @@
+
+/* 
+
+Challenge #1
+
 console.log('Before timeout');
 
 // Schedule a callback function to be executed after 3 seconds
@@ -5,6 +10,27 @@ setTimeout(() => {
   console.log('4311o'); // Log the first word
   console.log('th3r3'); // Log the second word
   console.log('w0r1d'); // Log the third word
+}, 3000);
+
+console.log('After timeout'); // Log a message after scheduling the timeout
+
+*/
+
+// Challenge #2
+
+console.log('Before timeout');
+
+// Schedule a callback function to be executed after 3 seconds
+setTimeout(() => {
+  console.log('4311o'); // Log the first word
+  // Schedule another callback function to be executed after 1 second
+  setTimeout(() => {
+    console.log('th3r3'); // Log the second word
+    // Schedule another callback function to be executed after 1 second
+    setTimeout(() => {
+      console.log('w0r1d'); // Log the third word
+    }, 1000);
+  }, 1000);
 }, 3000);
 
 console.log('After timeout'); // Log a message after scheduling the timeout
