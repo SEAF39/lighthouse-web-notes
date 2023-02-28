fb24c11eae88a085fdccf79a9f13013570cfaa7e
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-27 22:19:13 -0500

    update play file and move function to input.js - week5

--- week_5/day_1/networking/snake-client/play.js
+++ week_5/day_1/networking/snake-client/play.js
@@ -1,44 +1,7 @@
-const net = require("net");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host : 'localhost', // IP address here,
-    port : 50541 // PORT number here,
-  });
-
-  conn.on("connect", () => {
-    console.log("Connected to server!");
-    
-    conn.on("data", (data) => {
-      console.log("Received data from server:", data);
-    });
-  });
-  
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function () {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-};
-
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
+console.log("");
 connect();
+
+setupInput();
