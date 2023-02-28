2c4b6d4868a38dc878f1469e6abea5b690f7d5e3
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-27 21:49:05 -0500

    add setupInput & handlerInput - function - week5

--- week_5/day_1/networking/snake-client/play.js
+++ week_5/day_1/networking/snake-client/play.js
@@ -15,6 +15,25 @@ const connect = function () {
     });
   });
   
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function () {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
