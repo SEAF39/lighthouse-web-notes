75007cbbaff6ed060cfd4f122cfbc36a8dd9a42e
SEAF39 <labber@DESKTOP-7J1C764>
2023-02-27 22:26:06 -0500

    add WASD Movement - week5

--- week_5/day_1/networking/snake-client/input.js
+++ week_5/day_1/networking/snake-client/input.js
@@ -8,13 +8,21 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function () {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
+const handleUserInput = function (key) {
+  if (key === '\u0003') { // ctrl-c (end of text character)
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
+  }
 };
+
 
 
 module.exports = { setupInput };
