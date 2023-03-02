5ac0287d13b0416ee03696d8309a0ae160fd07e5
SEAF39 <labber@DESKTOP-7J1C764>
2023-03-01 19:23:04 -0500

    Api-jason The cat as services - week5

--- week_5/day_1/networking/api/json_the_cat/breedFetcher.js
+++ week_5/day_1/networking/api/json_the_cat/breedFetcher.js
@@ -6,9 +6,24 @@ const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 request(url, (error, response, body) => {
   if (error) {
-    console.log('Error fetching data!', error);
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
+    console.error('Error:', error);
+    return;
   }
+
+  const data = JSON.parse(body);
+
+  if (data.length === 0) {
+    console.log(`Breed "${breedName}" not found.`);
+    return;
+  }
+
+  const breed = data[0];
+  console.log(breed.description);
 });
+
+
+const data = JSON.parse(body);
+console.log(data);
+console.log(typeof data);
+
+module.exports = breedFetcher;
