496dc60d6bb4e7a9b12ed213a65cef9bd81940e8
SEAF39 <labber@DESKTOP-7J1C764>
2023-03-02 03:26:03 -0500

    Promises - ISS Spotter With Promises - week5

--- week_5/day_1/networking/promises/iss_spotter/index2.js
+++ week_5/day_1/networking/promises/iss_spotter/index2.js
@@ -4,4 +4,5 @@ const { fetchMyIP } = require('./iss_promised');
 
 fetchMyIP()
   .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
   .then(body => console.log(body));
--- week_5/day_1/networking/promises/iss_spotter/iss_promised.js
+++ week_5/day_1/networking/promises/iss_spotter/iss_promised.js
@@ -10,4 +10,14 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+
+
+
 module.exports = { fetchMyIP };
